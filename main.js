355513abb114d4207ad7082449a1f55ef4357355
Talaat <talaatgamal.dev@gmail.com>
2024-10-08 01:34:36 +0300

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const correctPassword = "porn";  // كلمة المرور الصحيحة
 
 function checkPassword() {
@@ -12,16 +13,23 @@ function checkPassword() {
         document.getElementById('password').placeholder ="Welcome to this website";
           // مسح أي حرف موجود في حقل الإدخال حتى يظهر الـ placeholder
           document.getElementById('password').value = "";  // مسح النص الموجود
-
-
+            
           sidebar.style.right = '-100%'; // إخفاء الشريط الجانبي
-
-
-    
+        sidebar.style.transition = 'right 3s ease';
+        
+        errorMsg.style.display = 'none';
     }
         else {
         // عرض رسالة خطأ إذا كانت كلمة المرور غير صحيحة
         errorMsg.style.display = 'block';
+
+        document.getElementById('password').placeholder ="Password is wrong, try again";
+        document.getElementById('password').value = "";  // مسح النص الموجود
+
+        // setTimeout(function() {
+        //     window.location.href = 'index.html'; // Replace with your desired URL
+        // }, 1000); // Delay of 2 seconds before redirection
+
     }
 }
 
@@ -35,28 +43,6 @@ document.getElementById('password').addEventListener('keydown', function(event) 
 
 
 
-// // الحصول على الأيقونة والشريط الجانبي
-// const menuIcon = document.getElementById('menuIcon');
-// const sidebar = document.getElementById('sidebar');
-
-// // إضافة حدث للنقر على الأيقونة
-// menuIcon.addEventListener('click', function() {
-//     // التبديل بين حالة إظهار الشريط الجانبي وإخفائه
-//     if (sidebar.style.right === '-250px') {
-//         sidebar.style.right = '0'; // إظهار الشريط الجانبي
-//     } else {
-//         sidebar.style.right = '-250px'; // إخفاء الشريط الجانبي
-//     }
-// });
-
-
-
-
-
-
-
-
-
 
 
 
@@ -64,24 +50,16 @@ document.getElementById('password').addEventListener('keydown', function(event) 
 const menuIcon = document.getElementById('menuIcon');
 const sidebar = document.getElementById('sidebar');
 
-// تعيين خاصية الانتقال
-sidebar.style.transition = 'right 0.4s ease';
-
 // إضافة حدث للنقر على الأيقونة
 menuIcon.addEventListener('click', function() {
     // التبديل بين حالة إظهار الشريط الجانبي وإخفائه
     if (sidebar.style.right === '-100%' || sidebar.style.right === '') {
         sidebar.style.right = '0'; // إظهار الشريط الجانبي
+        sidebar.style.transition = 'right 0.4s ease';
+
         // sidebar.style.display = 'block'; // تأكد من استخدام علامات الاقتباس
     } else {
         sidebar.style.right = '-100%'; // إخفاء الشريط الجانبي
     }
 });
-
-
-
-
-
-
-
 
